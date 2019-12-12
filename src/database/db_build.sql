ff7460286999dd75c0061d0a2dcbc1a93c105c6e
Pat Cichocki <pat.cichocki.uk@gmail.com>
2019-12-12 11:28:08 +0000

    database update

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -5,39 +5,42 @@ BEGIN;
 DROP TABLE IF EXISTS users, shop, basket CASCADE;
 
 CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  name varchar,
+  user_id SERIAL PRIMARY KEY,
+  username varchar,
   kitty integer DEFAULT 16,
   password varchar
 );
 
-INSERT INTO users (name) VALUES ('Ana');
-INSERT INTO users (name, kitty) VALUES ('Elsa', 80);
-INSERT INTO users (name, kitty, password) VALUES ('Olaf', 16, 'password')
+INSERT INTO users (username) VALUES ('Ana');
+INSERT INTO users (username, kitty) VALUES ('Elsa', 80);
+INSERT INTO users (username, kitty, password) VALUES ('Olaf', 16, 'password');
 
 CREATE TABLE IF NOT EXISTS shop (
-  id SERIAL PRIMARY KEY,
-  fruit varchar,
-  stock int,
-  price integer
+  fruit_id SERIAL PRIMARY KEY,
+  fruit_name varchar,
+  fruit_stock int,
+  fruit_price decimal(10, 2)
 );
 
-INSERT INTO shop (fruit, stock, price) VALUES ('Apples 6 pack', 100, 1.29);
-INSERT INTO shop (fruit, stock, price) VALUES ('Watermelon', 100, 3.50);
-INSERT INTO shop (fruit, stock, price) VALUES ('Mangoes x 2', 100, 1.29);
-INSERT INTO shop (fruit, stock, price) VALUES ('Bananas x 5', 100, 0.99);
-INSERT INTO shop (fruit, stock, price) VALUES ('Clementines multipack', 100, 1.69);
-INSERT INTO shop (fruit, stock, price) VALUES ('Pears 6 pack', 100, 1.49);
-INSERT INTO shop (fruit, stock, price) VALUES ('Pomelo', 100, 1.50);
-INSERT INTO shop (fruit, stock, price) VALUES ('Kiwis 6 pack', 100, 1.99);
-INSERT INTO shop (fruit, stock, price) VALUES ('Mangsteen 6 pack', 100, 5.99);
-INSERT INTO shop (fruit, stock, price) VALUES ('Dragon fruit 2 pack', 100, 4.99);
-INSERT INTO shop (fruit, stock, price) VALUES ('Tomatoes 6 pack', 100, 0.89);
+
+INSERT INTO shop(fruit_name, fruit_stock, fruit_price) VALUES ('Apple', 100, 1.29), 
+('Watermelon', 100, 3.50),
+('Mango', 100, 1.29),
+('Banana', 100, 0.99),
+('Clementine', 100, 1.69),
+('Pear', 100, 1.49),
+('Pomelo', 100, 1.50),
+('Kiwi', 100, 1.99),
+('Mangosteen', 100, 5.99),
+('Dragon fruit', 100, 4.99),
+('Tomato', 100, 0.89);
 
 CREATE TABLE IF NOT EXISTS basket (
-  id SERIAL PRIMARY KEY,
+  basket_id SERIAL PRIMARY KEY,
   fruit_id int,
-  stock int
+  fruit_price int,
+  purchase_quantity int,
+  purchase_amount decimal(10, 2)
 );
 
 -- ALTER TABLE "users" ADD FOREIGN KEY ("id") REFERENCES "basket" ("id");
