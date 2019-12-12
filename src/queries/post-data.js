1ccc7ba8648064528bcbf261d4c205747f765112
Pat Cichocki <pat.cichocki.uk@gmail.com>
2019-12-12 14:36:39 +0000

    getUser update

--- src/queries/post-data.js
+++ src/queries/post-data.js
@@ -2,12 +2,13 @@
 
 const dbConnection = require("../database/db_connection");
 
-const postName = (name, cb) => {
-    let nameSplit = name.split('%3D')[1];
+const postName = (username, password, cb) => {
+    const nameSplit = username.split('%3D')[1];
+    const userPassword = password.split('%3D')[1];
     dbConnection.query(
-        `INSERT INTO users (name) VALUES ($1)
+        `INSERT INTO users (username, password) VALUES ($1, $2)
         RETURNING kitty`,
-        [nameSplit],
+        [nameSplit, userPassword],
         (err, res) => {
             if (err) {
                 console.log(err);
